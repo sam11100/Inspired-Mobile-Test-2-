86fb9996e7f05b23bd5ca6302b2bd10227c9886c
Samir Thapa <sam11100@hotmail.co.uk>
2016-12-23 12:18:54 +0000

    test

---
+++ test-zurich/scripts/custom.js
@@ -0,0 +1,11 @@
+$(window).ready(function(){
+
+
+	$('.heading').delay(1000).fadeIn(1000);
+	$('.content').delay(2000).fadeIn(1000);
+	$('.tagline').delay(3000).fadeIn(1000);
+	$('.logo').delay(4000).fadeIn(1000);
+	$('.cta').delay(5000).fadeIn(1000);
+
+
+});
